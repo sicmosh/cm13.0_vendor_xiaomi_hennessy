a4b43a0234959e2f8d2bbeb2abd4e3ce57a8e9c4
Alex G <sicmosh@gmail.com>
2016-11-17 22:02:45 +0300

    Add files via upload

---
+++ hennessy-vendor-blobs.mk
@@ -0,0 +1,14 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/app,system/app)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/vendor,system/vendor)
